9ac0cc3a6c8d0eab7d6849223e4ae90e9c9c12d1
Dariushzz <dariush.lotfi@estudiant.fjaverianas.com>
2026-03-24 01:41:24 +0100

    URI

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://dariush_lotfi:QG28DJwXqfsUSyK0@cluster0.unczp2o.mongodb.net/products?retryWrites=true&w=majority"
+  mongoURI: process.env.MONGOURI
 };
